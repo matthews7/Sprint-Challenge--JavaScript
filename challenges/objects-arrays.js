d4ae6385857164e14b7e736b31e60c9aa152548d
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-11-15 09:42:41 -0700

    functions done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,55 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const trex = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: function(){
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+};
+
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const steg = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: ' 9m',
+  period: 'Late Jurassic',
+};
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const raptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar());
 
 
 // ==== Arrays ====
@@ -52,15 +80,25 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+  for (let i = 0; i < graduates.length; i++){
+    universities.push(graduates.universities)
+  }
+  universities.sort()
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
+
 
 The resulting contact information strings should have a space between the first name and the email, like this: 
 "Josh josh@example.com"
 
 Log the result of your new array. */
 const contactInfo = [];
+function getCarInfoByIndex(inventory, index) {
+  const string = `This is a ${inventory[index].car_make} ${inventory[index].car_model}`
+   return string
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
